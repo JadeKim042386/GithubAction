116480bb38f864ea3dc75b2f107fd1e92a0ef309
JadeKim042386 <jk042386@gmail.com>
2023-07-30 18:01:44 +0900

    Update Controller & deploy.yaml & application.yml

--- src/main/java/com/spring/githubaction/controller/DemoController.java
+++ src/main/java/com/spring/githubaction/controller/DemoController.java
@@ -1,12 +1,17 @@
 package com.spring.githubaction.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DemoController {
+    @Value("${override.value}")
+    private  String value;
+
     @GetMapping("/demo")
     public String demo() {
-        return "demo";
+        return "demo" + value;
     }
 }
